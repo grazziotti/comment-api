d378354315a42d5e1390c46aa6273b84cd5056e4
Daniel <dgplays14@gmail.com>
2023-12-08 23:55:08 -0300

    feat: complete user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,24 @@
 import { Router } from 'express'
 import { UserController } from '@/modules/user/user.controller'
 import { validateAuth } from '@/validators/validateAuth'
-import { createUserValidation } from '@/validators/userValidator'
+import {
+  createUserValidation,
+  editUserValidation,
+} from '@/validators/userValidator'
+import { privateRoute } from '@/config/passport'
 
 const userRoutes = Router()
 const userController = new UserController()
 
+userRoutes.get('/', privateRoute, userController.get)
 userRoutes.post('/', createUserValidation, validateAuth, userController.create)
+userRoutes.put(
+  '/',
+  privateRoute,
+  editUserValidation,
+  validateAuth,
+  userController.update,
+)
+userRoutes.delete('/', privateRoute, userController.delete)
 
 export default userRoutes
